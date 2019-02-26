b58863333207358291002b3bcde2e8db0e8af2f9
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-26 19:35:38 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,8 @@ class PostsController < ApplicationController
     post = Post.find(params[:id])
     # render json: PostSerializer.serialize(post)
     # render json: post.to_json
-    render json: post.to_json(include: :author)
+    # render json: post.to_json(include: :author)
+    
   end
 
   private
